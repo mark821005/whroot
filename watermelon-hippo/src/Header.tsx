af445cbf341845c7fa7b7581c5644c56e9402c74
Mark Adams <mark.adams82@gmail.com>
2025-07-27 17:06:24 +0100

    Header / Footer stlying

--- watermelon-hippo/src/Header.tsx
+++ watermelon-hippo/src/Header.tsx
@@ -7,8 +7,12 @@ import {
 
 export default function Header() {
   return (
-    <header className="fixed z-50 w-full">
-      <Navbar fluid rounded className="text-amber-100 dark:bg-neutral-800">
+    <header className="fixed top-4 left-[2vw] z-25 m-auto w-[96vw]">
+      <Navbar
+        fluid
+        rounded
+        className="text-amber-100 backdrop-blur-md dark:bg-neutral-800/80"
+      >
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
           Watermelon Hippo
         </span>
