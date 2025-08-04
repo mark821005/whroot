bf57f117ded012f8acdaa9b7c47c5870bfef40e6
Mark Adams <mark.adams82@gmail.com>
2025-08-04 18:33:51 +0100

    Removes footer links

--- watermelon-hippo/src/Components/Footer.tsx
+++ watermelon-hippo/src/Components/Footer.tsx
@@ -13,10 +13,7 @@ export default function Component() {
     <Footer className="mx-auto mb-8 w-[96vw] max-w-full p-4 text-amber-100 dark:bg-neutral-900">
       <FooterCopyright href="#" by="Watermelon Hippo" year={year} />
       <FooterLinkGroup>
-        <FooterLink href="#">About</FooterLink>
-        <FooterLink href="#">Privacy Policy</FooterLink>
-        <FooterLink href="#">Licensing</FooterLink>
-        <FooterLink href="#">Contact</FooterLink>
+        <FooterLink href="#"></FooterLink>
       </FooterLinkGroup>
     </Footer>
   );
