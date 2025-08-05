a6dfbb076b7cf600acc9ae4fe14b2be6110f4464
i-com-markadams <99671544+mark8210@users.noreply.github.com>
2025-08-05 11:23:29 +0100

    Adds mono font

--- watermelon-hippo/src/Components/Polaroids.tsx
+++ watermelon-hippo/src/Components/Polaroids.tsx
@@ -14,20 +14,22 @@ export default function Polaroids({ polaroids }: PropsTwo) {
   const [imageToShow, setImageToShow] = useState("");
   const [lightboxDisplay, setLightboxDisplay] = useState(false);
   const renderPolaroids = polaroids.map((polaroid) => (
-    <div
-      style={{
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ["transform" as any]: `rotate(${polaroid.rotate}deg)`,
-      }}
-      className="bg-stone-300 px-4 pt-4 pb-6 hover:bg-neutral-50"
-    >
-      <img
-        className="cursor-pointer"
-        src={polaroid.image}
-        alt=""
-        onClick={() => showImage(polaroid.image)}
-      />
-      <span className="font-mono text-amber-900">{polaroid.description}</span>
+    <div>
+      <div
+        style={{
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          ["transform" as any]: `rotate(${polaroid.rotate}deg)`,
+        }}
+        className="bg-stone-300 px-4 pt-4 pb-6 hover:bg-neutral-50"
+      >
+        <img
+          className="cursor-pointer"
+          src={polaroid.image}
+          alt=""
+          onClick={() => showImage(polaroid.image)}
+        />
+        <span className="font-mono text-amber-950">{polaroid.description}</span>
+      </div>
     </div>
   ));
 
