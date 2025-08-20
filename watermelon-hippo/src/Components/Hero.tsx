f5a55e9b5c5db1f68e475e7bcc07d665f4011705
mark8210 <99671544+mark8210@users.noreply.github.com>
2025-08-20 14:53:52 +0100

    Scroll effect on Hero compnent

--- watermelon-hippo/src/Components/Hero.tsx
+++ watermelon-hippo/src/Components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero({ titleone, titletwo, image, intro }: HeroProps) {
     <div
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       style={{ ["--image-url" as any]: `url(${image})` }}
-      className={`relative min-h-screen min-w-full bg-[image:var(--image-url)] bg-contain bg-top-right bg-no-repeat py-24`}
+      className={`hero relative min-h-screen min-w-full bg-[image:var(--image-url)] bg-contain bg-top-right bg-no-repeat py-24`}
     >
       <h1 className="ls mb-32 text-[17vw] leading-[13vw] tracking-tight">
         {titleone}
