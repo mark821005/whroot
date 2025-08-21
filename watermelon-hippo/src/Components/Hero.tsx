5ba0c53157fc1a1d7ca280b93f1a453e9ce9d31c
i-com-markadams <99671544+mark8210@users.noreply.github.com>
2025-08-21 21:29:08 +0100

    Hero scroll animation update

--- watermelon-hippo/src/Components/Hero.tsx
+++ watermelon-hippo/src/Components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero({ titleone, titletwo, image, intro }: HeroProps) {
     <div
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       style={{ ["--image-url" as any]: `url(${image})` }}
-      className={`hero relative min-h-screen min-w-full bg-[image:var(--image-url)] bg-contain bg-top-right bg-no-repeat py-24`}
+      className={`hero relative min-w-full bg-[image:var(--image-url)] bg-contain bg-top-right bg-no-repeat pt-24 pb-0 md:pb-8 xl:min-h-screen xl:pb-0`}
     >
       <h1 className="ls mb-32 text-[17vw] leading-[13vw] tracking-tight">
         {titleone}
